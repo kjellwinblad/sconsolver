6da75c47761bb7e8380cbfcd07b0859f370b63b1
Kjell Winblad <kjellwinblad@gmail.com>
2010-11-03 21:43:45 +0100

    Added a domain type that will work as domain for integer variables.

--- src/main/scala/me/winsh/scons/core/Domain.scala
+++ src/main/scala/me/winsh/scons/core/Domain.scala
@@ -10,62 +10,297 @@ import scala.collection.immutable.Iterable
  *
  */
 trait Domain extends Iterable[Int] {
-	
-	protected val domainRanges: List[Range]
-	
-	val min:Int
-    val max:Int
-    
-    def union(that:Domain):Domain
-	
+
+  protected val domainRanges: List[Range]
+
+  val min: Int
+
+  val max: Int
+
+  def union(that: Domain): Domain
+
+  def intersection(that: Domain): Domain
+
+  def difference(that: Domain): Domain
+
+  def onlyOneContains(that: Domain): Domain
+
+  def domainWithSmallestMinFirst(that: Domain): (Domain, Domain)
+
 }
 
 object Domain {
-	
-	def apply(min:Int,max:Int):Domain = {
-		require(min<=max, "The min parameter is bigger than the max parameter")
-		
-		new DomainImpl(List(min to max))
-	}
-	
-	def apply(domainValues:Iterable[Int]):Domain = {
-		
-		val domainValuesList = domainValues.toList.sort(_ < _)
-		
-		val ranges = domainValuesList.foldLeft(Nil:List[Range])((rangesList, value) => rangesList match{
-				case Nil => List(value to value)
-				case e::ls if e.max == value =>  e::ls
-				case e::ls if e.max == value + 1 =>  (e.min to value)::ls
-				case ls => (value to value)::ls
-			}).reverse
-		
-		
-		new DomainImpl(ranges)
-	}
-	
-	def apply(domainValues:Int*):Domain = apply(domainValues.toList)
+
+  val empty = this(Nil)
+
+  def apply(min: Int, max: Int): Domain = {
+    require(min <= max, "The min parameter is bigger than the max parameter")
+
+    new DomainImpl(List(min to max))
+  }
+
+  def apply(domainValues: Iterable[Int]): Domain = {
+
+    val domainValuesList = domainValues.toList.sort(_ < _)
+
+    val ranges = domainValuesList.foldLeft(Nil: List[Range])((rangesList, value) => rangesList match {
+      case Nil => List(value to value)
+      case e :: ls if e.max == value => e :: ls
+      case e :: ls if e.max + 1 == value => (e.min to value) :: ls
+      case ls => (value to value) :: ls
+    }).reverse
+
+    new DomainImpl(ranges)
+  }
+
+  def apply(domainValues: Int*): Domain = apply(domainValues.toList)
 
   private class DomainImpl(val domainRanges: List[Range]) extends Domain {
 
-    val min = domainRanges.first.min
-    
-    val max = domainRanges.last.max
+    lazy val min = {
+      require(domainRanges != Nil)
+      domainRanges.first.min
+    }
 
-    def union(that:Domain):Domain = 
-    	if(this.max < that.min)
-    		new DomainImpl(this.domainRanges ++ that.domainRanges)
-    	else if(that.max < this.min)
-    		new DomainImpl(that.domainRanges ++ this.domainRanges)
-    	else{
-    		//They are intersecting in some way 
-    		//Find out which ranges are intersecting and change them
-    		
-    		
-    		new DomainImpl(that.domainRanges ++ this.domainRanges) //TODO
-    		
-    	}
-    	
-    
+    lazy val max = {
+      require(domainRanges != Nil)
+      domainRanges.last.max
+    }
+
+    def union(that: Domain): Domain =
+      if (this.max < that.min)
+        new DomainImpl(concatenateOrderedRangesIfAdjecent(this.domainRanges ++ that.domainRanges))
+      else if (that.max < this.min)
+        new DomainImpl(concatenateOrderedRangesIfAdjecent(that.domainRanges ++ this.domainRanges))
+      else {
+
+        def union(subtractFrom: List[Range],
+          subtract: List[Range],
+          result: List[Range]): Domain = (subtractFrom, subtract) match {
+          case (Nil, domain) =>
+            new DomainImpl(concatenateOrderedRangesIfAdjecent((domain.reverse ::: result).reverse))
+          case (domain, Nil) =>
+            new DomainImpl(concatenateOrderedRangesIfAdjecent((domain.reverse ::: result).reverse))
+          case (set1First :: set1Second :: set1Rest, set2) if (rangesHasIntersection(set1First, set1Second)) =>
+            union((set1First.min.min(set1Second.min) to set1First.max.max(set1Second.max)) :: set1Rest, set2, result)
+          case (set1First :: set1Rest, set2First :: set2Rest) if (set1First.max < set2First.min) =>
+            union(set1Rest, set2First :: set2Rest, set1First :: result)
+          case (set1First :: set1Rest, set2First :: set2Rest) if (set2First.max < set1First.min) =>
+            union(set1First :: set1Rest, set2Rest, set2First :: result)
+          case (set1First :: set1Rest, set2First :: set2Rest) =>
+            union((set1First.min.min(set2First.min) to set1First.max.max(set2First.max)) :: set1Rest, set2Rest, result)
+
+        }
+
+        union(this.domainRanges, that.domainRanges, Nil)
+
+      }
+
+    def intersection(that: Domain): Domain =
+      if (this == empty || that == empty)
+        empty
+      else if (this.max < that.min)
+        empty
+      else if (that.max < this.min)
+        empty
+      else {
+        //They may be intersecting in some way 
+        //Find out which ranges are intersecting and change them
+
+        def intersectionDomain(smallestMinDomainbigestMinDomain: (Domain, Domain),
+          result: List[Range]): Domain = smallestMinDomainbigestMinDomain match {
+          case (Domain.empty, _) => new DomainImpl(result.reverse)
+          case (_, Domain.empty) => new DomainImpl(result.reverse)
+          case (smallestMinDomain, bigestMinDomain) => {
+            val checkRanges = smallestMinDomain.domainRanges
+
+            val firstInBigestMinDomain = bigestMinDomain.domainRanges.first
+
+            val checkedRanges =
+              checkRanges.dropWhile((range) => range.min < firstInBigestMinDomain.min &&
+                !rangesHasIntersection(range, firstInBigestMinDomain))
+
+            checkedRanges match {
+              case Nil => new DomainImpl(result.reverse)
+              case intersectingRange :: rest if (rangesHasIntersection(intersectingRange, firstInBigestMinDomain)) => {
+
+                val newResultRange = rangeIntersection(intersectingRange, firstInBigestMinDomain)
+
+                //Do recursive call with the intersecting range droped in in
+                //firstElementIntersectingRanges and in bigestMinDomain
+
+                val newDomain1 = intersectingRange.max match {
+                  case value if (value == newResultRange.max) => new DomainImpl(checkedRanges.drop(1))
+                  case _ => new DomainImpl(((newResultRange.max + 1) to intersectingRange.max) :: checkedRanges.drop(1))
+                }
+
+                val newDomain2 = firstInBigestMinDomain.max match {
+                  case value if (value == newResultRange.max) => new DomainImpl(bigestMinDomain.domainRanges.drop(1))
+                  case _ => new DomainImpl(((newResultRange.max + 1) to firstInBigestMinDomain.max) :: bigestMinDomain.domainRanges.drop(1))
+                }
+
+                intersectionDomain(newDomain1.domainWithSmallestMinFirst(newDomain2),
+                  newResultRange :: result)
+              }
+              case _ => {
+
+                //In this case it is not certain that checkedRanges don't intersect with
+                //the second range in bigestMinDomain.domainRanges so we can just drop the
+                //first element in bigestMinDomain.domainRanges
+
+                val newDomain1 = new DomainImpl(checkedRanges)
+
+                val newDomain2 = new DomainImpl(bigestMinDomain.domainRanges.drop(1))
+
+                intersectionDomain(newDomain1.domainWithSmallestMinFirst(newDomain2),
+                  result)
+
+              }
+            }
+          }
+        }
+
+        intersectionDomain(this.domainWithSmallestMinFirst(that), Nil)
+
+      }
+
+    def difference(that: Domain): Domain =
+      if (this == empty ||
+        that == empty ||
+        this.max < that.min ||
+        that.max < this.min)
+        this
+      else {
+        def difference(subtractFrom: List[Range],
+          subtract: List[Range],
+          result: List[Range]): Domain = (subtractFrom, subtract) match {
+          case (Nil, domain) =>
+            new DomainImpl(result.reverse)
+          case (domain, Nil) =>
+            new DomainImpl((domain.reverse ::: result).reverse)
+          case (subtractFrom :: subtractFromRest, subtract :: subtractRest) if (subtractFrom.min > subtract.max) =>
+            difference(subtractFrom :: subtractFromRest, subtractRest, result)
+          case (subtractFrom :: subtractFromRest, subtract :: subtractRest) if (subtractFrom.max < subtract.min) =>
+            difference(subtractFromRest, subtract :: subtractRest, subtractFrom :: result)
+          case (subtractFrom :: subtractFromRest, subtract :: subtractRest) =>
+            (subtractFrom, subtract) match {
+
+              case (f, s) if (f.min >= s.min && s.max > f.max) =>
+                difference(subtractFromRest, (f.max + 1 to s.max) :: subtractRest, result)
+              case (f, s) if (f.min >= s.min && s.max == f.max) =>
+                difference(subtractFromRest, subtractRest, result)
+              case (f, s) if (f.min >= s.min && s.max < f.max) =>
+                difference((s.max + 1 to f.max) :: subtractFromRest, subtractRest, result)
+              case (f, s) if (f.min < s.min && s.max > f.max) =>
+                difference(subtractFromRest, (f.max + 1 to s.max) :: subtractRest, (f.min to s.min - 1) :: result)
+              case (f, s) if (f.min < s.min && s.max == f.max) =>
+                difference(subtractFromRest, subtractRest, (f.min to s.min - 1) :: result)
+              case (f, s) if (f.min < s.min && s.max < f.max) =>
+                (f.min to s.min - 1) :: Nil
+                difference((s.max + 1 to f.max) :: subtractFromRest, subtractRest, (f.min to s.min - 1) :: result)
+
+            }
+
+        }
+
+        difference(this.domainRanges, that.domainRanges, Nil)
+      }
+
+    def onlyOneContains(that: Domain): Domain =
+      if (this == empty)
+        that
+      else if (that == empty)
+        this
+      else if (this.max < that.min)
+        new DomainImpl(concatenateOrderedRangesIfAdjecent(this.domainRanges ::: that.domainRanges))
+      else if (that.max < this.min)
+        new DomainImpl(concatenateOrderedRangesIfAdjecent(that.domainRanges ::: this.domainRanges))
+      else {
+        //They may be intersecting in some way 
+        //Find out which ranges are intersecting and change them
+
+        def onlyOneContains(smallestMinDomainbigestMinDomain: (Domain, Domain),
+          result: List[Range]): Domain = smallestMinDomainbigestMinDomain match {
+          case (Domain.empty, domain) =>
+            new DomainImpl(concatenateOrderedRangesIfAdjecent((domain.domainRanges.reverse ::: result).reverse))
+          case (domain, Domain.empty) =>
+            new DomainImpl(concatenateOrderedRangesIfAdjecent((domain.domainRanges.reverse ::: result).reverse))
+          case (smallestMinDomain, bigestMinDomain) => {
+            val checkRanges = smallestMinDomain.domainRanges
+
+            val firstInBigestMinDomain = bigestMinDomain.domainRanges.first
+
+            val (differ, mayDiffer) =
+              checkRanges.span((range) => range.min < firstInBigestMinDomain.min &&
+                !rangesHasIntersection(range, firstInBigestMinDomain))
+
+            mayDiffer match {
+              case Nil => new DomainImpl((differ.reverse ::: result).reverse)
+              case intersectingRange :: rest if (intersectingRange.min == firstInBigestMinDomain.min &&
+                intersectingRange.max == firstInBigestMinDomain.max) => {
+
+                val newDomain1 = new DomainImpl(mayDiffer.drop(1))
+
+                val newDomain2 = new DomainImpl(bigestMinDomain.domainRanges.drop(1))
+
+                onlyOneContains(newDomain1.domainWithSmallestMinFirst(newDomain2),
+                  differ.reverse ::: result)
+              }
+              case intersectingRange :: rest if (rangesHasIntersection(intersectingRange, firstInBigestMinDomain)) => {
+
+                val newResultRanges = (intersectingRange, firstInBigestMinDomain) match {
+                  case (r1, r2) if (r1.min == r2.min) => (r1.max.min(r2.max) + 1 to r1.max.max(r2.max)) :: Nil
+                  case (r1, r2) if (r1.max == r2.max) => (r1.min.min(r2.min) to r1.min.max(r2.min) - 1) :: Nil
+                  case (r1, r2) => (r1.max.min(r2.max) + 1 to r1.max.max(r2.max)) :: (r1.min.min(r2.min) to r1.min.max(r2.min) - 1) :: Nil
+                }
+
+                val maxNewResultRange = newResultRanges.first
+                //Do recursive call with the intersecting range droped in in
+                //firstElementIntersectingRanges and in bigestMinDomain
+
+                val (newResultRangesToReturn, newDomain1, newDomain2) =
+                  (intersectingRange.max, firstInBigestMinDomain.max) match {
+                    case (iMax, fMax) if (iMax == fMax) =>
+                      (newResultRanges,
+                        new DomainImpl(mayDiffer.drop(1)),
+                        new DomainImpl(bigestMinDomain.domainRanges.drop(1)))
+                    case (iMax, fMax) if (iMax > fMax) =>
+                      (if (newResultRanges.size == 2) newResultRanges.drop(1) else Nil,
+                        new DomainImpl(maxNewResultRange :: mayDiffer.drop(1)),
+                        new DomainImpl(bigestMinDomain.domainRanges.drop(1)))
+                    case _ =>
+                      (if (newResultRanges.size == 2) newResultRanges.drop(1) else Nil,
+                        new DomainImpl(mayDiffer.drop(1)),
+                        new DomainImpl(maxNewResultRange :: bigestMinDomain.domainRanges.drop(1)))
+                  }
+
+                onlyOneContains(newDomain1.domainWithSmallestMinFirst(newDomain2),
+                  newResultRangesToReturn ::: differ.reverse ::: result)
+              }
+              case _ => {
+
+                val newDomain1 = new DomainImpl(mayDiffer)
+
+                val newDomain2 = new DomainImpl(bigestMinDomain.domainRanges)
+
+                onlyOneContains(newDomain1.domainWithSmallestMinFirst(newDomain2),
+                  differ.reverse ::: result)
+
+              }
+            }
+          }
+        }
+
+        onlyOneContains(this.domainWithSmallestMinFirst(that), Nil)
+
+      }
+
+    def domainWithSmallestMinFirst(that: Domain) = (this, that) match {
+      case (Domain.empty, _) => (this, that)
+      case (_, Domain.empty) => (this, that)
+      case _ => if (this.min <= that.min) (this, that) else (that, this)
+    }
+
     val iterator = new Iterator[Int] {
 
       var domainRangesPos = 0
@@ -89,16 +324,40 @@ object Domain {
       def hasNext = domainRanges.size > domainRangesPos
 
     }
-    
-    private def rangeUnion(range1:Range, range2:Range) = 1 to 1	//TODO
-    
-    private def rangeIntersection(range1:Range, range2:Range) = 1 to 1 //TODO	
-    
+
+    override def equals(that: Any): Boolean = {
+      that.isInstanceOf[Domain] && this.domainRanges == that.asInstanceOf[Domain].domainRanges
+    }
+
+    //Private helper functions
+
+    private def concatenateOrderedRangesIfAdjecent(ranges: List[Range]) = {
+      def concatenateRangesIfAdjecentInternal(ranges: List[Range],
+        result: List[Range]): List[Range] = ranges match {
+        case Nil => result.reverse
+        case e :: Nil => (e :: result).reverse
+        case e1 :: e2 :: rest if e1.max == (e2.min - 1) =>
+          concatenateRangesIfAdjecentInternal((e1.min to e2.max) :: rest, result)
+        case e1 :: rest =>
+          concatenateRangesIfAdjecentInternal(rest, e1 :: result)
+
+      }
+      concatenateRangesIfAdjecentInternal(ranges, Nil)
+    }
+
+    private def rangeIntersection(range1: Range, range2: Range) =
+      if (rangesHasIntersection(range1, range2))
+        range1.min.max(range2.min) to range1.max.min(range2.max)
+      else
+        emptyRange
+
     private val emptyRange = 0 until 0
-    
-    private def rangesHasIntersection(range1:Range, range2:Range) =
-    	range1.contains(range2.min) || range1.contains(range2.max) ||
-    	range2.contains(range1.min) || range2.contains(range1.max)
+
+    private def rangesHasIntersection(range1: Range, range2: Range) =
+      range1.contains(range2.min) ||
+        range1.contains(range2.max) ||
+        range2.contains(range1.min) ||
+        range2.contains(range1.max)
   }
 
 }
--- src/test/scala/me/winsh/scons/DomainTest.scala
+++ src/test/scala/me/winsh/scons/DomainTest.scala
@@ -10,6 +10,7 @@ class DomainTest {
   @Test
   def iterateOverNewDomain() = {
 
+	   
     {
       val domain = Domain(4, 10)
 
@@ -54,6 +55,235 @@ class DomainTest {
       assertEquals((1 to 10 by 2).toList, iteratedVals)
     }
 
+  }
+  
+  @Test
+  def equals() = {
+	  assertEquals(Domain(1,2,3), Domain(1 to 3))
+	  
+	  assertEquals(Domain(1,2,3,9,10,11), Domain((1 to 3).union(9 to 11)))
+	  
+	  assertTrue(Domain(1,2,3,4,9,10,11) != Domain((1 to 3).union(9 to 11)))
+  }
+  
+  @Test
+  def intersection() = {
+	
+	  //Everything intersects
+	/*{
+      val domain1 = Domain(1 to 10)
+      
+      val domain2 = Domain(1 to 10)
+
+      assertEquals(domain1, domain1.intersection(domain2))
+      
+      assertEquals(domain1, domain2.intersection(domain1))
+    }
+	
+		  //A part of simple domains intersects
+	{
+      val domain1 = Domain(1 to 10)
+      
+      val domain2 = Domain(5 to 15)
+
+      assertEquals(Domain(5 to 10), domain1.intersection(domain2))
+      
+      assertEquals(Domain(5 to 10), domain2.intersection(domain1))
+    }*/
+	
+	//Complex intersection 1
+	{
+	  val set1 = Set() ++ (1 to 10).union(30 to 40).union(50 to 60)
+      val domain1 = Domain(set1)
+      
+      val set2 = Set() ++ (35 to 37).union(40 to 40).union(45 to 65)
+      val domain2 = Domain(set2)
+
+      assertEquals(Domain(set1.intersect(set2)), domain1.intersection(domain2))
+      
+      assertEquals(Domain(set1.intersect(set2)), domain2.intersection(domain1))
+    }
+	
+		//Complex intersection 2
+	{
+	  val set1 = Set() ++ (1 to 15 by 2).union(30 to 40).union(50 to 60)
+      val domain1 = Domain(set1)
+      
+      val set2 = Set() ++ (2 to 7 by 2).union(10 to 16).union(35 to 37).union(40 to 40).union(45 to 65)
+      val domain2 = Domain(set2)
+
+      assertEquals(Domain(set1.intersect(set2)), domain1.intersection(domain2))
+      
+      assertEquals(Domain(set1.intersect(set2)), domain2.intersection(domain1))
+    }
+	  
+  
+  }
+  
+      @Test
+  def difference() = {
+	
+	  //Everything intersects
+	{
+      val domain1 = Domain(1 to 10)
+      
+      val domain2 = Domain(1 to 10)
+      
+      assertEquals(Domain(), domain1.difference(domain2))
+      
+      assertEquals(Domain(), domain2.difference(domain1))
+    }
+	
+		  //A part of simple domains intersects
+	{
+      val domain1 = Domain(1 to 10)
+      
+      val domain2 = Domain(5 to 15)
+
+      assertEquals(Domain(1 to 4), domain1.difference(domain2))
+      
+      assertEquals(Domain(11 to 15), domain2.difference(domain1))
+    }
+	
+	//Complex intersection 1
+	{
+	  val set1 = Set() ++ (1 to 10).union(30 to 40).union(50 to 60)
+      val domain1 = Domain(set1)
+      
+      val set2 = Set() ++ (35 to 37).union(40 to 40).union(45 to 65)
+      val domain2 = Domain(set2)
+     
+      assertEquals(Domain(set1.diff(set2)), domain1.difference(domain2))
+      
+      assertEquals(Domain(set2.diff(set1)), domain2.difference(domain1))
+    } 
+	
+		//Complex intersection 2
+	{
+	  val set1 = Set() ++ (1 to 15 by 2).union(30 to 40).union(50 to 60)
+      val domain1 = Domain(set1)
+      
+      val set2 = Set() ++ (2 to 7 by 2).union(10 to 16).union(35 to 37).union(40 to 40).union(45 to 65)
+      val domain2 = Domain(set2)
+
+      
+      assertEquals(Domain(set1.diff(set2)), domain1.difference(domain2))
+      
+      assertEquals(Domain(set2.diff(set1)), domain2.difference(domain1))
+    }
+	  
+  
+  }
+  
+      
+      @Test
+  def union() = {
+	
+	  //Everything intersects
+	{
+      val domain1 = Domain(1 to 10)
+      
+      val domain2 = Domain(1 to 10)
+
+      assertEquals(Domain(1 to 10), domain1.union(domain2))
+      
+      assertEquals(Domain(1 to 10), domain2.union(domain1))
+    }
+	
+		  //A part of simple domains intersects
+	{
+      val domain1 = Domain(1 to 10)
+      
+      val domain2 = Domain(5 to 15)
+
+      assertEquals(Domain(1 to 15), domain1.union(domain2))
+      
+      assertEquals(Domain(1 to 15), domain2.union(domain1))
+    }
+	
+	//Complex intersection 1
+	{
+	  val set1 = Set() ++ (1 to 10).union(30 to 40).union(50 to 60)
+      val domain1 = Domain(set1)
+      
+      val set2 = Set() ++ (35 to 37).union(40 to 40).union(45 to 65)
+      val domain2 = Domain(set2)
+     
+      //assertEquals(Domain(set1.union(set2)), domain1.union(domain2))
+      
+      assertEquals(Domain(set2.union(set1)), domain2.union(domain1))
+    } 
+	
+		//Complex intersection 2
+	{
+	  val set1 = Set() ++ (1 to 15 by 2).union(30 to 40).union(50 to 60)
+      val domain1 = Domain(set1)
+      
+      val set2 = Set() ++ (2 to 7 by 2).union(10 to 16).union(35 to 37).union(40 to 40).union(45 to 65)
+      val domain2 = Domain(set2)
+
+      
+      assertEquals(Domain(set1.union(set2)), domain1.union(domain2))
+      
+      assertEquals(Domain(set2.union(set1)), domain2.union(domain1))
+    }
+	  
+  
+  }
+  
+    @Test
+  def onlyOneContains() = {
+	
+	  //Everything intersects
+	{
+      val domain1 = Domain(1 to 10)
+      
+      val domain2 = Domain(1 to 10)
+
+      assertEquals(Domain(), domain1.onlyOneContains(domain2))
+      
+      assertEquals(Domain(), domain2.onlyOneContains(domain1))
+    }
+	
+		  //A part of simple domains intersects
+	{
+      val domain1 = Domain(1 to 10)
+      
+      val domain2 = Domain(5 to 15)
+
+      assertEquals(Domain((1 to 4).union(11 to 15)), domain1.onlyOneContains(domain2))
+      
+      assertEquals(Domain((1 to 4).union(11 to 15)), domain2.onlyOneContains(domain1))
+    }
+	
+	//Complex intersection 1
+	{
+	  val set1 = Set() ++ (1 to 10).union(30 to 40).union(50 to 60)
+      val domain1 = Domain(set1)
+      
+      val set2 = Set() ++ (35 to 37).union(40 to 40).union(45 to 65)
+      val domain2 = Domain(set2)
+     
+      assertEquals(Domain(set1.diff(set2).union(set2.diff(set1))), domain1.onlyOneContains(domain2))
+      
+      assertEquals(Domain(set2.diff(set1).union(set1.diff(set2))), domain2.onlyOneContains(domain1))
+    } 
+	
+		//Complex intersection 2
+	{
+	  val set1 = Set() ++ (1 to 15 by 2).union(30 to 40).union(50 to 60)
+      val domain1 = Domain(set1)
+      
+      val set2 = Set() ++ (2 to 7 by 2).union(10 to 16).union(35 to 37).union(40 to 40).union(45 to 65)
+      val domain2 = Domain(set2)
+
+      
+      assertEquals(Domain(set1.diff(set2).union(set2.diff(set1))), domain1.onlyOneContains(domain2))
+      
+      assertEquals(Domain(set2.diff(set1).union(set1.diff(set2))), domain2.onlyOneContains(domain1))
+    }
+	  
+  
   }
 
   //    @Test
